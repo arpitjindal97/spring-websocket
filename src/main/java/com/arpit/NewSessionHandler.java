e27f8f7d116cd723776894b749fe0ee0617d64e0
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-05-09 00:12:40 +0530

    no need of db

--- src/main/java/com/arpit/NewSessionHandler.java
+++ src/main/java/com/arpit/NewSessionHandler.java
@@ -1,8 +1,8 @@
 package com.arpit;
 
-import com.hazelcast.core.Hazelcast;
+/*import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
+import com.hazelcast.core.IMap;*/
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -19,10 +19,12 @@ import java.util.Map;
 public class NewSessionHandler extends TextWebSocketHandler {
     private static final Logger logger = LoggerFactory.getLogger(NewSessionHandler.class);
 
-    public HazelcastInstance hzInstance;
+    //public HazelcastInstance hzInstance;
 
     //record of current sessions
     Map<String, WebSocketSession> map = new HashMap<String, WebSocketSession>();
+    Map<String,String> alternate;
+    Map<String,String> mappings = new HashMap<String,String>();
 
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 
@@ -42,7 +44,7 @@ public class NewSessionHandler extends TextWebSocketHandler {
             //pass the message to appropriate person
             try {
 
-                Map<String, String> mappings = hzInstance.getMap("mappings");
+                //Map<String, String> mappings = hzInstance.getMap("mappings");
 
                 TextMessage new_mes = new TextMessage(
                         "<p class=\"strangermsg\"><strong class=\"msgsource\">Stranger:</strong> <span>" + message.getPayload() + "</span></p>");
@@ -73,7 +75,7 @@ public class NewSessionHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws IOException {
-        logger.info("Connection established ID: "+session.getId());
+        //logger.info("Connection established ID: "+session.getId());
 
         map.put(session.getId(), session);
 
@@ -81,7 +83,7 @@ public class NewSessionHandler extends TextWebSocketHandler {
         session.sendMessage(new TextMessage("<p class='statuslog'>Finding some stranger...</p>"));
         session.sendMessage(new TextMessage("server: textarea disabled"));
 
-        IMap<String,String> alternate = hzInstance.getMap("alternate");
+        //IMap<String,String> alternate = hzInstance.getMap("alternate");
 
 
         for(Map.Entry entry: alternate.entrySet()) {
@@ -93,7 +95,7 @@ public class NewSessionHandler extends TextWebSocketHandler {
                 alternate.put("key","1");
 
                 //put yourself in db and wait for someone
-                Map<String, String> mappings = hzInstance.getMap("mappings");
+                //Map<String, String> mappings = hzInstance.getMap("mappings");
 
                 mappings.put(session.getId(), "null");
                 //client will be able to find partner in next request
@@ -115,25 +117,25 @@ public class NewSessionHandler extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         try {
             String id = session.getId();
-            logger.info("Connection closed ID: " + id);
+            //logger.info("Connection closed ID: " + id);
             map.remove(session.getId());
 
 
-            Map<String,String> alternate = hzInstance.getMap("mappings");
+            //Map<String,String> alternate = hzInstance.getMap("mappings");
 
             String stranger_id="";
-            for(Map.Entry entry : alternate.entrySet())
+            for(Map.Entry entry : mappings.entrySet())
             {
                 if(entry.getKey().equals(id) )
                 {
                     stranger_id = entry.getValue().toString();
-                    alternate.remove(entry);
+                    mappings.remove(entry);
                     break;
                 }
                 if(entry.getValue().equals(id))
                 {
                     stranger_id = entry.getKey().toString();
-                    alternate.remove(entry);
+                    mappings.remove(entry);
                     break;
                 }
             }
@@ -157,24 +159,24 @@ public class NewSessionHandler extends TextWebSocketHandler {
 
     public void setConnection() {
 
-        hzInstance = Hazelcast.newHazelcastInstance();
-        Map<String,Integer> alternate = hzInstance.getMap("alternate");
-        alternate.put("key",1);
+        //hzInstance = Hazelcast.newHazelcastInstance();
+        alternate = new HashMap<String,String>();
+        alternate.put("key","1");
     }
 
     public void search_stranger(WebSocketSession session, int key) {
         try {
-            Map<String,String> alternate = hzInstance.getMap("mappings");
+            //Map<String,String> alternate = hzInstance.getMap("mappings");
 
             if (key == 1) {
                 //search someone from db
 
                 //search one time only
-                for(Map.Entry entry : alternate.entrySet())
+                for(Map.Entry entry : mappings.entrySet())
                 {
                     if(entry.getValue().equals("null"))
                     {
-                        alternate.put(entry.getKey().toString(),session.getId());
+                        mappings.put(entry.getKey().toString(),session.getId());
 
                         //will only be executed after finding partner
                         session.sendMessage(new TextMessage(
@@ -193,7 +195,7 @@ public class NewSessionHandler extends TextWebSocketHandler {
 
                 //check if someone is sitting beside
 
-                String value = alternate.get(session.getId());
+                String value = mappings.get(session.getId());
 
                 if (value.equals("null")) {
                     session.sendMessage(new TextMessage("server: didt found"));
--- src/main/java/com/arpit/WebSocketConfig.java
+++ src/main/java/com/arpit/WebSocketConfig.java
@@ -1,8 +1,5 @@
 package com.arpit;
 
-import com.hazelcast.config.Config;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
